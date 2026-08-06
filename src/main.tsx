69c74840f8d72fc4dd29978f0e120191064d9d5d
Senuja Bodhinayake <senuja.bodhinayake@ascentic.se>
2026-08-06 14:57:41 +0530

    feat: wire MSW service worker into app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
+async function enableMocking() {
+  const { worker } = await import('./mocks/browser');
+  worker.start({onUnhandledRequest: 'bypass'});
+}
+
+enableMocking().then(() => {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
+});
